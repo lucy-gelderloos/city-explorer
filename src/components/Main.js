e8ac73b24b7561a633ce07af4c2e98c4565e6551
Lucy Gelderloos <lucygel@gmail.com>
2022-07-18 21:48:05 -0700

    Add alert

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,7 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-
+import Alert from 'react-bootstrap/Alert';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 // import axios from 'axios';
@@ -16,6 +16,8 @@ class Main extends React.Component {
     constructor(props){
         super(props);
         this.state={
+            city: {},
+            searchFor: '',
             cityName: 'Seattle',
             latitude: 48,
             longitude: 122,
@@ -24,6 +26,7 @@ class Main extends React.Component {
         this.apiKey = process.env.REACT_APP_API_KEY;
         this.searchUrl = "https://us1.locationiq.com/v1/search.php?format=json&";
         this.cities = props.cities;
+        this.blankSearch = true;
     }
 
     
@@ -33,16 +36,29 @@ class Main extends React.Component {
 
     handleSubmit = (event) => {
         event.preventDefault();
-        this.handleSearchCity(this.state.searchFor);
+        // demo of error message; replace when API is working
+        if(this.state.searchFor === ''){
+            this.blankSearch = true;
+            console.log(this.blankSearch);
+            return this.blankSearch;
+        }
+        else{
+            this.handleSearchCity(this.state.searchFor);
+        }
     }
     
     handleSearchCity = (searchFor) => {
         // const API = `${this.searchURL}key=${this.apiKey}&q=${this.cityName}}&format=json`;
-        // const res = await axios.get(API);
-        // console.log(res.data[0])
-        // this.setState({ location:res.data[0] });
+        // axios.get(API);
+        // .then(res => {
+        // console.log(res.data[0]);
+        // this.setState({ city:res.data[0] });
+        // })
+        // .catch(err => {
+        // this.setState({success:false})
+        // })
 
-        const testSearch = this.cities.filter(el => el.cityName === searchFor);
+        const testSearch = this.cities.filter(el => el.cityName.toLowerCase() === searchFor.toLowerCase());
         this.setState({ cityName:testSearch[0].cityName, latitude:testSearch[0].lat, longitude:testSearch[0].lon})
     }
 
@@ -50,6 +66,7 @@ class Main extends React.Component {
         console.log(this.state);
         return (
         <div className="Main">
+            <Alert show={this.blankSearch} onClose={() => this.blankSearch = false} dismissible>Please enter Seattle, Olympia, or Portland</Alert>
             <Row>
                 <Col id="mainInfo">
                     <div id="searchForm">
