9c41ee9f31797c47355794c3d512e876acadd099
Lucy Gelderloos <lucygel@gmail.com>
2022-07-20 19:33:55 -0700

    Troubleshooting Weather component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import Header from './components/Header.js';
 import Main from './components/Main.js';
 import Footer from './components/Footer.js';
 import React from 'react';
-import cities from './cities.json';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main cities={cities} />
+      <Main />
       <Footer />
     </div>
   );
---
+++ src/components/Weather.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import axios from 'axios';
+import Alert from 'react-bootstrap/Alert';
+
+class Weather extends React.Component {
+    constructor(props) {
+        super(props);
+        this.cityName = props.cityName;
+        this.lat = Math.round(props.lat);
+        this.lon = Math.round(props.lon);
+        this.weatherSearchUrl = 'http://localhost:3030/weather?';
+        this.state = {  
+            error:'',
+            forecast:[]
+            }
+    }    
+    
+    getWeather = () => {
+        const weatherQuery = `${this.weatherUrl}cityName=${this.cityName}&lat=${this.lat}&lon=${this.lon}`;
+        console.log('weatherQuery',weatherQuery);
+        axios.get(weatherQuery)
+        .then(response => {
+            this.setState({forecast:response});
+        })
+        .catch(err => {
+            console.log(err);
+            this.setState({error:`Sorry, I don't have the weather for that city! Please enter Seattle, Amman, or Paris.`});
+            // eventually, this shouldn't be triggered by the same conditions as the invalid locationIQ search error - either add another state or move the whole weather process into a component & deal with it there
+        })
+    }
+
+    render() {
+        return(
+            <div>
+            <Alert show={this.state.error} onClose={() => this.setState({error:false})} dismissible>{this.state.error}</Alert>
+            <p>this.lon {this.lon}, forecast {this.forecast}</p>
+            {/* <p>{this.getWeather()}</p> */}
+            </div>
+        )
+
+    }
+}
+
+export default Weather;
